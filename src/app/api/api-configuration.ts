c1db8fbf836caf20808dcac24fc887ba3df97755
emanuele-toma <toma.emanuele03@gmail.com>
2025-03-20 15:11:13 +0100

    refactor: Change API baseUrl

--- src/app/api/api-configuration.ts
+++ src/app/api/api-configuration.ts
@@ -11,7 +11,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfiguration {
-  rootUrl: string = 'http://{{base_url}}';
+  rootUrl: string = 'https://server-angular-jac.dev.aws.r-s.cloud/api';
 }
 
 /**
