187fe6948141d265ce3a951586bab0a59cd8a9d7
emanuele-toma <toma.emanuele03@gmail.com>
2025-03-27 15:18:03 +0100

    feat: Implement authentication guard for protected routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,32 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, type Routes } from "@angular/router";
-import { LoginComponent } from "./core/auth/login/login.component";
-import { RegisterComponent } from "./core/auth/register/register.component";
-import { NotFoundComponent } from "./shared/components/not-found/not-found.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, type Routes } from '@angular/router';
+import { LoginComponent } from './core/auth/login/login.component';
+import { RegisterComponent } from './core/auth/register/register.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-	{
-		path: "login",
-		component: LoginComponent,
-	},
-	{
-		path: "register",
-		component: RegisterComponent,
-	},
-	{
-		path: "**",
-		component: NotFoundComponent,
-	},
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'app',
+    canActivate: [authGuard],
+    children: [],
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
