6f340ddb1a10aab7b9a922c89accffe47dd5e3bb
emanuele-toma <toma.emanuele03@gmail.com>
2025-03-27 15:10:06 +0100

    :passport_control: Add auth interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptors([authInterceptor])),
+  ],
 };
