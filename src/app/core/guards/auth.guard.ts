6e39fc1d15ac0edbe4da8e58b34bf5e66e71468f
emanuele-toma <toma.emanuele03@gmail.com>
2025-04-17 12:03:25 +0200

    feat: Enhance auth guard to redirect unauthenticated users to login

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,10 +1,21 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { map } from 'rxjs';
 import { TokenService } from '../services/token.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+
   const tokenService = inject(TokenService);
 
-  return tokenService.token$.pipe(map(token => !!token));
+  const isAuthenticated = tokenService.token$.pipe(map(token => !!token));
+  return isAuthenticated.pipe(
+    map(isAuth => {
+      if (!isAuth) {
+        router.navigate(['/login']);
+        return false;
+      }
+      return true;
+    })
+  );
 };
