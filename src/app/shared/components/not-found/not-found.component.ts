35c4574213349a030b937261dbba2b6faf1586c1
emanuele-toma <toma.emanuele03@gmail.com>
2025-04-17 10:16:49 +0200

    feat: Enhance Not Found component with improved UI and navigation

--- src/app/shared/components/not-found/not-found.component.ts
+++ src/app/shared/components/not-found/not-found.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatIconModule, MatButtonModule, RouterModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
 })
